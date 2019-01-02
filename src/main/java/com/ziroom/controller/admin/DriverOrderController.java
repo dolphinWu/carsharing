d479887da53061cca2229396539802f68b01f17d
liuzh <liuzehua_work>
2019-01-02 16:20:24 +0800

    添加一个controller类--liuzh--0102

---
+++ src/main/java/com/ziroom/controller/admin/DriverOrderController.java
@@ -0,0 +1,15 @@
+package com.ziroom.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Date:2019/1/2 16:14
+ * @Author: liuzh
+ * @Description:
+ */
+@Controller
+@RequestMapping("driverOrder")
+public class DriverOrderController {
+
+}
