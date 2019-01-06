3fa404c08802f8534abda92f6746b1647e6ee1ef
wuhy1 <wuhy1@mail.ziroom.com>
2019-01-06 16:36:50 +0800

    司机端|修改 planAmount类型-----wuhy1----0106----v18

--- src/main/java/com/ziroom/model/DriverPlanEntity.java
+++ src/main/java/com/ziroom/model/DriverPlanEntity.java
@@ -1,8 +1,13 @@
 package com.ziroom.model;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class DriverPlanEntity extends BaseEntity {
 
     private Integer id;
@@ -13,8 +18,7 @@ public class DriverPlanEntity extends BaseEntity {
 
     private String remark;
 
-    //单位分
-    private Integer planAmount;
+    private Double planAmount;
 
     private Integer status;
 
@@ -36,123 +40,4 @@ public class DriverPlanEntity extends BaseEntity {
 
     private String endName;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getPlanStartTime() {
-        return planStartTime;
-    }
-
-    public void setPlanStartTime(Date planStartTime) {
-        this.planStartTime = planStartTime;
-    }
-
-    public Integer getCarCapacity() {
-        return carCapacity;
-    }
-
-    public void setCarCapacity(Integer carCapacity) {
-        this.carCapacity = carCapacity;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public Integer getPlanAmount() {
-        return planAmount;
-    }
-
-    public void setPlanAmount(Integer planAmount) {
-        this.planAmount = planAmount;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getDriverUid() {
-        return driverUid;
-    }
-
-    public void setDriverUid(String driverUid) {
-        this.driverUid = driverUid;
-    }
-
-    public String getDriverNo() {
-        return driverNo;
-    }
-
-    public void setDriverNo(String driverNo) {
-        this.driverNo = driverNo;
-    }
-
-    public String getStartXpoint() {
-        return startXpoint;
-    }
-
-    public void setStartXpoint(String startXpoint) {
-        this.startXpoint = startXpoint;
-    }
-
-    public String getStartYpoint() {
-        return startYpoint;
-    }
-
-    public void setStartYpoint(String startYpoint) {
-        this.startYpoint = startYpoint;
-    }
-
-    public String getEndXpoint() {
-        return endXpoint;
-    }
-
-    public void setEndXpoint(String endXpoint) {
-        this.endXpoint = endXpoint;
-    }
-
-    public String getEndYpoint() {
-        return endYpoint;
-    }
-
-    public void setEndYpoint(String endYpoint) {
-        this.endYpoint = endYpoint;
-    }
-
-    public Integer getAccountingRules() {
-        return accountingRules;
-    }
-
-    public void setAccountingRules(Integer accountingRules) {
-        this.accountingRules = accountingRules;
-    }
-
-    public String getStartName() {
-        return startName;
-    }
-
-    public void setStartName(String startName) {
-        this.startName = startName;
-    }
-
-    public String getEndName() {
-        return endName;
-    }
-
-    public void setEndName(String endName) {
-        this.endName = endName;
-    }
 }
